439e65426dc288d3210588ac31d7c00e73f0828c
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-03-31 16:17:32 +0000

    Remove Gemfile

--- src/main/java/com/phylogeny/extrabitmanipulation/api/jei/CycleTimer.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/api/jei/CycleTimer.java
@@ -9,3 +9,35 @@ import net.minecraft.client.gui.GuiScreen;
 /**
  * This class is taken by permission from JEI.
  *
+ * @author mezz
+ */
+public class CycleTimer
+{
+	/* the amount of time in ms to display one thing before cycling to the next one */
+	private static final int cycleTime = 1000;
+	private long startTime = 0;
+	private long drawTime = 0;
+	private long pausedDuration = 0;
+	
+	public CycleTimer(int offset)
+	{
+		long time = System.currentTimeMillis();
+		this.startTime = time - (offset * cycleTime);
+		this.drawTime = time;
+	}
+	
+	@Nullable
+	public <T> T getCycledItem(List<T> list)
+	{
+		if (list.isEmpty())
+			return null;
+		
+		Long index = ((drawTime - startTime) / cycleTime) % list.size();
+		return list.get(index.intValue());
+	}
+	
+	public void onDraw()
+	{
+		if (!GuiScreen.isShiftKeyDown())
+		{
+			if (pausedDuration 
