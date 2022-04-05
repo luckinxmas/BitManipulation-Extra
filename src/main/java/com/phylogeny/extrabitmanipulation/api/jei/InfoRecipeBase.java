14fea5033e780550de09aa086faaa5e8352b100e
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-04-05 09:57:24 +0000

    merge l10n

--- src/main/java/com/phylogeny/extrabitmanipulation/api/jei/InfoRecipeBase.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/api/jei/InfoRecipeBase.java
@@ -13,4 +13,17 @@ import mezz.jei.api.recipe.IRecipeWrapper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
-import com.phylogeny.ext
+import com.phylogeny.extrabitmanipulation.reference.Reference;
+
+public class InfoRecipeBase implements IRecipeWrapper
+{
+	protected List<ItemStack> itemStacks;
+	protected List<String> tooltipLines = new ArrayList<String>();
+	protected ResourceLocation image;
+	protected int imageWidth, imageHeight;
+	protected IDrawable slotDrawable;
+	protected String name, text;
+	protected Rectangle imageBox;
+	
+	public InfoRecipeBase(IGuiHelper guiHelper, List<ItemStack> itemStacks, int imageWidth, int imageHeight, String recipeName,
+			String imageName, String tooltipName, int imageLeft, int imageTop, int imageRight, int imageBottom, String catagoryName
