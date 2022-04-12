e9620accd5616bd0517ec191bb3b0da739fe81d8
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-04-12 11:29:45 +0000

    cifs: extend the buffer length enought for sprintf() using

--- src/main/java/com/phylogeny/extrabitmanipulation/api/jei/Translator.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/api/jei/Translator.java
@@ -2,4 +2,35 @@ package com.phylogeny.extrabitmanipulation.api.jei;
 
 import java.util.IllegalFormatException;
 
-import net.minecraft.uti
+import net.minecraft.util.text.translation.I18n;
+
+import com.phylogeny.extrabitmanipulation.helper.LogHelper;
+
+/**
+ * This class is taken by permission from JEI.
+ *
+ * @author mezz
+ */
+@SuppressWarnings("deprecation")
+public final class Translator
+{
+	private Translator() {}
+	
+	public static String translateToLocal(String key)
+	{
+		if (I18n.canTranslate(key))
+			return I18n.translateToLocal(key);
+		
+		return I18n.translateToFallback(key);
+	}
+	
+	public static String translateToLocalFormatted(String key, Object... format)
+	{
+		String s = translateToLocal(key);
+		try
+		{
+			return String.format(s, format);
+		}
+		catch (IllegalFormatException e)
+		{
+		
