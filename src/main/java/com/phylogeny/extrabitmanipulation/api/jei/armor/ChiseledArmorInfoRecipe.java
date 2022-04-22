f1b17062affbd1b901342f8fb44b06f99e693114
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-04-22 12:24:57 +0000

    wording fix

--- src/main/java/com/phylogeny/extrabitmanipulation/api/jei/armor/ChiseledArmorInfoRecipe.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/api/jei/armor/ChiseledArmorInfoRecipe.java
@@ -15,4 +15,14 @@ import com.phylogeny.extrabitmanipulation.init.KeyBindingsExtraBitManipulation;
 
 public class ChiseledArmorInfoRecipe extends InfoRecipeBase
 {
-	publi
+	public static final String[] GRAPHIC_NAMES = new String[]{"chiseled_helmet", "creation", "collection", "chiseled_armor_gui", "chiseled_armor_slots_gui"};
+	private int imageIndex;
+	
+	public static List<ChiseledArmorInfoRecipe> create(IGuiHelper guiHelper, List<ItemStack> sculptingStacks)
+	{
+		List<ChiseledArmorInfoRecipe> recipes = new ArrayList<ChiseledArmorInfoRecipe>();
+		for (int i = 0; i < GRAPHIC_NAMES.length; i++)
+		{
+			recipes.add(new ChiseledArmorInfoRecipe(guiHelper, sculptingStacks, ChiseledArmorInfoRecipeCategory.NAME,
+					GRAPHIC_NAMES[i], i == 0 ? 89 : (i == 3 ? 76 : (i == 4 ? 75 : 78)), i == 0 ? 21 : (i == 3 ? 32 : (i == 4 ? 41 : 19)),
+							i == 0 ? 187 : (i == 3 ? 186 : (i ==
