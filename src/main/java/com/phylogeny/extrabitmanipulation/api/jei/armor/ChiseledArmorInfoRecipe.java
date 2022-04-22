633a4ff661b22197c19ea22a28cefc1e7da160c7
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-04-22 15:11:34 +0000

    release 0.6.2

--- src/main/java/com/phylogeny/extrabitmanipulation/api/jei/armor/ChiseledArmorInfoRecipe.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/api/jei/armor/ChiseledArmorInfoRecipe.java
@@ -25,4 +25,17 @@ public class ChiseledArmorInfoRecipe extends InfoRecipeBase
 		{
 			recipes.add(new ChiseledArmorInfoRecipe(guiHelper, sculptingStacks, ChiseledArmorInfoRecipeCategory.NAME,
 					GRAPHIC_NAMES[i], i == 0 ? 89 : (i == 3 ? 76 : (i == 4 ? 75 : 78)), i == 0 ? 21 : (i == 3 ? 32 : (i == 4 ? 41 : 19)),
-							i == 0 ? 187 : (i == 3 ? 186 : (i ==
+							i == 0 ? 187 : (i == 3 ? 186 : (i == 4 ? 183 : 186)), i == 0 ? 127 : (i == 3 ? 108 : (i == 4 ? 103 : 125)), i));
+		}
+		return recipes;
+	}
+	
+	public ChiseledArmorInfoRecipe(IGuiHelper guiHelper, List<ItemStack> sculptingStacks, String catagoryName,
+			String imageName, int imageLeft, int imageTop, int imageRight, int imageBottom, int imageIndex)
+	{
+		super(guiHelper, sculptingStacks, 0, 0, imageName, imageName.toLowerCase().replace(" ", "_"), ChiseledArmorInfoRecipeCategory.NAME + "." + imageName,
+				imageLeft, imageTop, imageRight, imageBottom, catagoryName);
+		this.imageIndex = imageIndex;
+		for (int i = 0; i < tooltipLines.size(); i++)
+			tooltipLines.set(i, tooltipLines.get(i).replace("@",
+					KeyBindingsExtraBitManipula
