a392ee92a27f9f8e989673798d91f410a02b78e0
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-06-02 10:29:41 +0000

    javascript

--- src/main/java/com/phylogeny/extrabitmanipulation/armor/ArmorItem.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/armor/ArmorItem.java
@@ -5,4 +5,18 @@ import java.util.List;
 
 import mod.chiselsandbits.api.ItemType;
 import net.minecraft.client.Minecraft;
-import 
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+import com.phylogeny.extrabitmanipulation.api.ChiselsAndBitsAPIAccess;
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+import com.phylogeny.extrabitmanipulation.reference.Configs;
+import com.phylogeny.extrabitmanipulation.reference.NBTKeys;
+
+public class ArmorItem
+{
+	private List<GlOperation> glOperations = new ArrayList<GlOperation>();
+	private ItemStack stack;
