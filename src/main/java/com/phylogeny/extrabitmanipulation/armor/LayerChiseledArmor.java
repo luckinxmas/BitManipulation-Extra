dad4791521e8480150bac012c7b81e7c3bb7cc65
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-06-16 16:55:12 +0000

    Update language

--- src/main/java/com/phylogeny/extrabitmanipulation/armor/LayerChiseledArmor.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/armor/LayerChiseledArmor.java
@@ -15,4 +15,19 @@ import com.phylogeny.extrabitmanipulation.init.ReflectionExtraBitManipulation;
 import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor;
 import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor.ArmorType;
 import com.phylogeny.extrabitmanipulation.reference.Configs;
-import com.phylogeny.extrabitmanipula
+import com.phylogeny.extrabitmanipulation.reference.CustomNPCsReferences;
+import com.phylogeny.extrabitmanipulation.reference.MorePlayerModelsReference;
+import com.phylogeny.extrabitmanipulation.reference.NBTKeys;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelIllager;
+import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.model.ModelVex;
+import net.minecraft.client.model.ModelVillager;
+import net.minecraft.client.renderer.GLAllocation;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
+import net.minecraft.
