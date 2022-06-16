48e8bea9752190409f5b705245ffc0d5508e2907
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-06-16 16:46:12 +0000

    [travis-ci] Retry composer install if fail

--- src/main/java/com/phylogeny/extrabitmanipulation/armor/LayerChiseledArmor.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/armor/LayerChiseledArmor.java
@@ -4,3 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import com.phylogeny.extrabitmanipulation.armor.capability.ChiseledArmorSlotsHandler;
+import com.phylogeny.extrabitmanipulation.armor.capability.IChiseledArmorSlotsHandler;
+import com.phylogeny.extrabitmanipulation.client.ClientHelper;
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+import com.phylogeny.extrabitmanipulation.init.ReflectionExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor;
+import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor.ArmorType;
+import com.phylogeny.extrabitmanipulation.reference.Configs;
+import com.phylogeny.extrabitmanipula
