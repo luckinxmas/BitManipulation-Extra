dcb5f7d869f302e7086a328eadfcb7bd7be1ffe9
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-09-07 06:30:37 +0000

    changed naming

--- src/main/java/com/phylogeny/extrabitmanipulation/armor/model/cnpc/ModelChiseledArmorBaseCNPC.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/armor/model/cnpc/ModelChiseledArmorBaseCNPC.java
@@ -8,4 +8,28 @@ import com.phylogeny.extrabitmanipulation.reference.Configs;
 
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
-import nop
+import noppes.npcs.client.model.ModelBipedAlt;
+import noppes.npcs.client.model.ModelScaleRenderer;
+import noppes.npcs.constants.EnumParts;
+
+
+public class ModelChiseledArmorBaseCNPC extends ModelBipedAlt
+{
+	protected float scale;
+	
+	public ModelChiseledArmorBaseCNPC(float scale)
+	{
+		super(scale);
+		textureWidth = 86;
+		textureHeight = 64;
+		scale = Configs.armorZFightingBufferScale;
+	}
+	
+	protected void setRotationAngles(ModelRenderer modelRenderer, float angleX, float angleY, float angleZ)
+	{
+		modelRenderer.rotateAngleX = angleX;
+		modelRenderer.rotateAngleY = angleY;
+		modelRenderer.rotateAngleZ = angleZ;
+	}
+	
+	protected ModelScaleRenderer createScaledModel(int texOffX, 
