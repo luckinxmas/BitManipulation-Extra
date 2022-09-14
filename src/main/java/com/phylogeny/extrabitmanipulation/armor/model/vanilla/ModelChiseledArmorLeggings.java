2d8f1aea5c9a9672dd4dd5b3ae15385db6db70ea
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-09-14 11:03:02 +0000

    documentation edits

---
+++ src/main/java/com/phylogeny/extrabitmanipulation/armor/model/vanilla/ModelChiseledArmorLeggings.java
@@ -0,0 +1,30 @@
+
+package com.phylogeny.extrabitmanipulation.armor.model.vanilla;
+
+import net.minecraft.client.model.ModelRenderer;
+
+import com.phylogeny.extrabitmanipulation.reference.Configs;
+
+public class ModelChiseledArmorLeggings extends ModelChiseledArmorBase
+{
+	
+	public ModelChiseledArmorLeggings()
+	{
+		super();
+		
+		//Pelvis
+		bipedBody = new ModelRenderer(this, 0, 45);
+		bipedBody.addBox(-4.0F, 7.0F, -2.0F, 8, 5, 4, scale);
+		
+		//Right Leg
+		bipedRightLeg = new ModelRenderer(this, 25, 40);
+		bipedRightLeg.setRotationPoint(-1.9F, 12.0F, 0.0F);
+		bipedRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 10, 4, scale + Configs.armorZFightingBufferScaleRightLegOrFoot);
+		
+		//Left Leg
+		bipedLeftLeg = new ModelRenderer(this, 25, 40);
+		bipedLeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
+		bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 10, 4, scale);
+	}
+	
+}
---
+++ src/main/java/com/phylogeny/extrabitmanipulation/block/BlockBodyPartTemplate.java
@@ -0,0 +1,7 @@
+package com.phylogeny.extrabitmanipulation.block;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.block.material.Mat
