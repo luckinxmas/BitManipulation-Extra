8d68ad0ab7138f0c83bfb9b6ef8345c4202738c6
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-09-14 19:59:47 +0000

    Fixed a broken link

--- src/main/java/com/phylogeny/extrabitmanipulation/block/BlockBodyPartTemplate.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/block/BlockBodyPartTemplate.java
@@ -4,4 +4,24 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-import net.minecraft.block.material.Mat
+import net.minecraft.block.material.Material;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockBodyPartTemplate extends BlockExtraBitManipulationBase
+{
+	
+	public BlockBodyPartTemplate(String name)
+	{
+		super(Material.GROUND, name);
+		setHardness(0.2F);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced)
+	{
+		tooltip.add("The bits of this block are used as bodypart placeholders in the creation of chisel
