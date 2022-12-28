3c18fedd372a9f6e06e16a58011c89099936c6de
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2022-12-28 08:41:51 +0000

    tty: serial: imx: don't reinit clock in imx_setup_ufcr()

--- src/main/java/com/phylogeny/extrabitmanipulation/client/ClientHelper.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/ClientHelper.java
@@ -6,4 +6,33 @@ import net.minecraft.client.renderer.BlockModelShapes;
 import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.client.renderer.entity.RenderManager;
-i
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IThreadListener;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+
+public class ClientHelper
+{
+	
+	private static Minecraft getMinecraft()
+	{
+		return Minecraft.getMinecraft();
+	}
+	
+	public static IThreadListener getThreadListener()
+	{
+		return getMinecraft();
+	}
+	
+	public static World getWorld()
+	{
+		return getMinecraft().world;
+	}
+	
+	public static EntityPlayer getPlayer()
+	{
+		return getMinec
