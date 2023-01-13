cb2f7713a4eb7523970c3520b8c92ddfc9f81cd8
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-01-13 11:57:43 +0000

    nfsd: fix rare symlink decoding bug

--- src/main/java/com/phylogeny/extrabitmanipulation/client/Prism.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/Prism.java
@@ -18,4 +18,23 @@ public class Prism extends Quadric
 	public void draw(float radius, boolean isOpen)
 	{
 		float slope = isPryamid ? radius : 0;
-		float 
+		float slope2 = isTriangular ? radius : 0;
+		boolean isCube = !isPryamid && !isTriangular;
+		GlStateManager.pushMatrix();
+		GlStateManager.translate(0, radius, isCube ? -radius : 0);
+		drawSquare(radius, isPryamid, slope, slope2);
+		GlStateManager.translate(0, -radius * 2, 0);
+		GlStateManager.scale(1, -1, 1);
+		drawSquare(radius, isPryamid, slope, slope2);
+		GlStateManager.popMatrix();
+		
+		GlStateManager.rotate(90, 0, 0, 1);
+		
+		GlStateManager.pushMatrix();
+		GlStateManager.translate(0, radius, isCube ? -radius : 0);
+		if (!isTriangular)
+		{
+			drawSquare(radius, isPryamid, slope, 0);
+		}
+		GlStateManager.translate(0, -radius * 2, 0);
+		
