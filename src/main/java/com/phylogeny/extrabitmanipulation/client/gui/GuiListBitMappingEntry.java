3dac0bca0e34085ec2c22f28d96f8a1f437fba91
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-01-17 16:44:30 +0000

    Update link in README

---
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/GuiListBitMappingEntry.java
@@ -0,0 +1,8 @@
+package com.phylogeny.extrabitmanipulation.client.gui;
+
+import java.util.ArrayList;
+
+import mod.chiselsandbits.api.APIExceptions.InvalidBitItem;
+import mod.chiselsandbits.api.IBitBrush;
+import mod.chiselsandbits.api.IChiselAndBitsAPI;
+import net.minecraf
