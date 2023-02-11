a2f52b476b9195f35205bb8ca6d3f2a455d93349
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-02-11 16:28:04 +0000

    Updated sql files

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiButtonArmorSlots.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiButtonArmorSlots.java
@@ -5,4 +5,16 @@ import net.minecraft.client.audio.SoundHandler;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
-import net.
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import com.phylogeny.extrabitmanipulation.ExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.client.ClientHelper;
+import com.phylogeny.extrabitmanipulation.client.gui.button.GuiButtonBase;
+import com.phylogeny.extrabitmanipulation.client.render.RenderState;
+import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
+import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor.ArmorMovingPart;
+import com.phylogeny.extrabitmanipulation.packet.PacketOpenInvento
