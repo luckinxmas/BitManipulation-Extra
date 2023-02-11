9777198bf50dcbe1e48faefebd559c81ad18512e
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-02-11 19:55:59 +0000

    Update INSTRUCTIONS.md

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiButtonArmorSlots.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiButtonArmorSlots.java
@@ -17,4 +17,23 @@ import com.phylogeny.extrabitmanipulation.client.gui.button.GuiButtonBase;
 import com.phylogeny.extrabitmanipulation.client.render.RenderState;
 import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
 import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor.ArmorMovingPart;
-import com.phylogeny.extrabitmanipulation.packet.PacketOpenInvento
+import com.phylogeny.extrabitmanipulation.packet.PacketOpenInventoryGui;
+
+public class GuiButtonArmorSlots extends GuiButtonBase
+{
+	private GuiContainer gui;
+	private int mouseInitialX, offsetX, offsetY, posX, posY;
+	
+	public GuiButtonArmorSlots(GuiContainer gui, String buttonText)
+	{
+		super(384736845, 0, 0, 12, 10, buttonText, "");
+		setHoverHelpText("While holding SHIFT + CONTROL + ALT:\n" + GuiChiseledArmor.getPointSub("1) ") +
+				"Click & drag to change position.\n" + GuiChiseledArmor.getPointSub("2) ") + "Press R to reset position.");
+		this.gui = gui;
+		setPosition();
+	}
+	
+	public void setPosition()
+	{
+		resetOffsets();
+		Pair<Integer, Integer> pos = BitToolSettingsHelper.getArmorButtonPosition(
