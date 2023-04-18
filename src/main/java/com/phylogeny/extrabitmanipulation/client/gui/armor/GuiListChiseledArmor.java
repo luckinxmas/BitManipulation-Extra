aee75044c70be057ace525a911d10ef3e08323cd
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-04-18 20:29:59 +0000

    af9005: fix kernel panic on init if compiled without IR

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiListChiseledArmor.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiListChiseledArmor.java
@@ -16,4 +16,18 @@ import org.lwjgl.opengl.GL11;
 
 import com.google.common.collect.Lists;
 import com.phylogeny.extrabitmanipulation.armor.DataChiseledArmorPiece;
-import com.phylogeny.ext
+import com.phylogeny.extrabitmanipulation.client.GuiHelper;
+
+public class GuiListChiseledArmor<E> extends GuiListExtended
+{
+	protected final GuiChiseledArmor guiChiseledArmor;
+	protected final List<GuiListEntryChiseledArmor<E>> entries = Lists.<GuiListEntryChiseledArmor<E>>newArrayList();
+	protected DataChiseledArmorPiece armorPiece;
+	private int selectedIndex = 0;
+	private boolean drawEntries;
+	
+	public GuiListChiseledArmor(GuiChiseledArmor guiChiseledArmor, int height,
+			int top, int bottom, int slotHeight, int offsetX, int listWidth, DataChiseledArmorPiece armorPiece)
+	{
+		super(guiChiseledArmor.mc, 150, height, top, bottom, slotHeight);
+		this.guiChiseledArmor = g
