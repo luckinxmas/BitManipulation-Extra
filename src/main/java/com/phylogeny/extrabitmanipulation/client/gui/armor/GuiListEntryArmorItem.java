14f9783c96bdd767bb130e29963710ed50bf40d9
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-04-24 20:04:10 +0000

    Backend

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiListEntryArmorItem.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiListEntryArmorItem.java
@@ -16,4 +16,20 @@ public class GuiListEntryArmorItem extends GuiListEntryChiseledArmor<ArmorItem>
 {
 	private boolean slotHovered;
 	
-	public GuiListEntryArmorItem(GuiListChiseledArmor<ArmorItem> listChiseledArmor, Arm
+	public GuiListEntryArmorItem(GuiListChiseledArmor<ArmorItem> listChiseledArmor, ArmorItem armorItem)
+	{
+		super(listChiseledArmor, armorItem);
+	}
+	
+	@Override
+	public void drawEntry(int slotIndex, int x, int y, int listWidth, int slotHeight, int mouseX, int mouseY, boolean isSelected, float partialTicks)
+	{
+		slotHovered = mouseX > x + 4 && mouseX < x + 23 && mouseY > y && mouseY < y + 19;
+		x += 5;
+		y += 1;
+		RenderHelper.enableGUIStandardItemLighting();
+		OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
+		ClientHelper.bindTexture(GuiChiseledArmor.TEXTURE_GUI);
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+		GlStateManager.disableLighting();
+		Gui.drawModalRectWithCustomSizedTexture(x, y, 0, 230, 18, 18,
