0271538e90abde62fb850a2a6c59eca2bb8a0d49
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-04-24 18:52:16 +0000

    Updated everything

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiListEntryArmorItem.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiListEntryArmorItem.java
@@ -1,4 +1,19 @@
 package com.phylogeny.extrabitmanipulation.client.gui.armor;
 
 import net.minecraft.client.gui.Gui;
-import net.mine
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.item.ItemStack;
+
+import com.phylogeny.extrabitmanipulation.ExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.armor.ArmorItem;
+import com.phylogeny.extrabitmanipulation.client.ClientHelper;
+import com.phylogeny.extrabitmanipulation.packet.PacketCursorStack;
+
+public class GuiListEntryArmorItem extends GuiListEntryChiseledArmor<ArmorItem>
+{
+	private boolean slotHovered;
+	
+	public GuiListEntryArmorItem(GuiListChiseledArmor<ArmorItem> listChiseledArmor, Arm
