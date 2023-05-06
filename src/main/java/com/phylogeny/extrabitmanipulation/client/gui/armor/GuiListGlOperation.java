28e6385b948c87dc2ce66bf55aa8efc8cebc23f8
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-05-06 08:21:34 +0000

    net: qmi_wwan: Add ID for Telewell TW-LTE 4G v2

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiListGlOperation.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/armor/GuiListGlOperation.java
@@ -5,4 +5,18 @@ import java.util.List;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.text.TextFormatting;
 
-import com.phylogeny.extrabitmanipulation.armor
+import com.phylogeny.extrabitmanipulation.armor.DataChiseledArmorPiece;
+import com.phylogeny.extrabitmanipulation.armor.GlOperation;
+
+public abstract class GuiListGlOperation<L> extends GuiListChiseledArmor<GlOperation>
+{
+	private static final String[] glComponents = new String[]{TextFormatting.BOLD + "x", TextFormatting.BOLD + "y", TextFormatting.BOLD + "z", "\u03B1"};
+	
+	public GuiListGlOperation(GuiChiseledArmor guiChiseledArmor, int height, int top,
+			int bottom, int slotHeight, int offsetX, int listWidth, DataChiseledArmorPiece armorPiece)
+	{
+		super(guiChiseledArmor, height, top, bottom, slotHeight, offsetX, listWidth, armorPiece);
+		setHasListHeader(true, 11);
+	}
+	
+	public abstract List<GlOperation> getGlOperatio
