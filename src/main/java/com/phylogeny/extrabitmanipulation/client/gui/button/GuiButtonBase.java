c228692b1a51c1ba198221e33932b9fb60ef9202
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-05-08 12:38:31 +0000

    Nada

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/button/GuiButtonBase.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/button/GuiButtonBase.java
@@ -12,4 +12,19 @@ import net.minecraft.client.audio.SoundHandler;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.util.SoundEvent;
 
-import 
+import com.phylogeny.extrabitmanipulation.init.SoundsExtraBitManipulation;
+
+public class GuiButtonBase extends GuiButton
+{
+	public boolean selected;
+	private boolean silent, helpMode;
+	private List<String> hoverText, hoverTextSelected, hoverHelpText;
+	private SoundEvent soundSelect, soundDeselect;
+	
+	public GuiButtonBase(int buttonId, int x, int y, int width, int height, String text, String hoverText)
+	{
+		this(buttonId, x, y, width, height, text, hoverText, null, null);
+	}
+	
+	public GuiButtonBase(int buttonId, int x, int y, int widthIn, int heightIn, String text,
+			String hoverText, @Nullable SoundEvent soundSelect, @Nullable SoundEvent soun
