a8a9c5ca708e1e9dd6a784a379e1623318e8e451
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-05-13 05:44:22 +0000

    [l10n] Updated Estonian translation

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/button/GuiButtonCustom.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/button/GuiButtonCustom.java
@@ -3,4 +3,32 @@ package com.phylogeny.extrabitmanipulation.client.gui.button;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 
-public class GuiButtonCustom e
+public class GuiButtonCustom extends GuiButtonBase
+{
+	protected float textOffsetX, textOffsetY;
+	
+	public GuiButtonCustom(int buttonId, int x, int y, int widthIn, int heightIn, String text, String hoverText)
+	{
+		super(buttonId, x, y, widthIn, heightIn, text, hoverText);
+	}
+	
+	public void setTextOffsetX(float textOffsetX)
+	{
+		this.textOffsetX = textOffsetX;
+	}
+	
+	public void setTextOffsetY(float textOffsetY)
+	{
+		this.textOffsetY = textOffsetY;
+	}
+	
+	protected void drawCustomRect() {}
+	
+	@Override
+	public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks)
+	{
+		if (!visible)
+			return;
+		
+		super.drawButton(mc, mouseX, mouseY, partialTicks);
+		GlStateManager.enableBlend(
