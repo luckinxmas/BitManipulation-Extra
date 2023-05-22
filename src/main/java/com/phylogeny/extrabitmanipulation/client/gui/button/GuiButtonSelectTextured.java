bf2c6f08d35cd785dceda37d5eaf5f98ec6f75fb
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-05-22 23:07:25 +0000

    Removed unused function

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/button/GuiButtonSelectTextured.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/button/GuiButtonSelectTextured.java
@@ -10,4 +10,29 @@ import com.phylogeny.extrabitmanipulation.client.GuiHelper;
 public class GuiButtonSelectTextured extends GuiButtonSelect
 {
 	private ResourceLocation texture;
-	private float rightOff
+	private float rightOffsetX;
+	private boolean shiftRight;
+	private static int scaleFactor;
+	
+	public GuiButtonSelectTextured(int buttonId, int x, int y, int width, int height, String text,
+			String hoverText, int colorFirst, int colorSecond, ResourceLocation texture)
+	{
+		super(buttonId, x, y, width, height, text, hoverText, colorFirst, colorSecond);
+		this.texture = texture;
+		scaleFactor = GuiHelper.getScaleFactor();
+	}
+	
+	public void setRightOffsetX(float textureOffsetX, boolean shiftRight)
+	{
+		this.rightOffsetX = textureOffsetX;
+		this.shiftRight = shiftRight;
+	}
+	
+	private void shiftRight()
+	{
+		if (shiftRight)
+			GlStateManager.translate(rightOffsetX, 0, 0);
+	}
+	
+	@Override
+	protected void d
