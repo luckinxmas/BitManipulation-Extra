695590327b146ede3cd1e0c166c62beb5132f4a8
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-06-01 06:22:29 +0000

    formbuilder with lblpos="T" now aligns labels to the left

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/button/GuiButtonTextured.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/button/GuiButtonTextured.java
@@ -4,4 +4,16 @@ import javax.annotation.Nullable;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.util.Resource
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+
+import com.phylogeny.extrabitmanipulation.client.ClientHelper;
+import com.phylogeny.extrabitmanipulation.client.GuiHelper;
+import com.phylogeny.extrabitmanipulation.init.SoundsExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.reference.Reference;
+
+public class GuiButtonTextured extends GuiButtonBase
+{
+	private static final ResourceLocation BOX_CHECKED = new ResourceLocation(Reference.MOD_ID, "textures/guis/box_checked.png");
+	private static final ResourceLocation BOX_UNCHECKED = new ResourceLocation(Reference.MOD_ID, "textures/guis/box_unchecked.png");
+	private ResourceLocation selectedTexture,
