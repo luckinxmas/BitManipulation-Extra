845d7ba80c32bbf10dc0629f5b898eed5a48e3c2
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-06-02 12:45:44 +0000

    Removed unnecessary dependency.

--- src/main/java/com/phylogeny/extrabitmanipulation/client/gui/button/GuiButtonTextured.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/gui/button/GuiButtonTextured.java
@@ -16,4 +16,22 @@ public class GuiButtonTextured extends GuiButtonBase
 {
 	private static final ResourceLocation BOX_CHECKED = new ResourceLocation(Reference.MOD_ID, "textures/guis/box_checked.png");
 	private static final ResourceLocation BOX_UNCHECKED = new ResourceLocation(Reference.MOD_ID, "textures/guis/box_unchecked.png");
-	private ResourceLocation selectedTexture,
+	private ResourceLocation selectedTexture, deselectedTexture;
+	
+	public GuiButtonTextured(int buttonId, int x, int y, int width, int height, String hoverText, ResourceLocation selectedTexture,
+			ResourceLocation deselectedTexture, @Nullable SoundEvent boxCheck, @Nullable SoundEvent boxUncheck)
+	{
+		super(buttonId, x, y, width, height, "", hoverText, boxCheck, boxUncheck);
+		this.selectedTexture = selectedTexture;
+		this.deselectedTexture = deselectedTexture;
+	}
+	
+	@Override
+	public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks)
+	{
+		super.drawButton(mc, mouseX, mouseY, partialTicks);
+		if (!visible)
+			return;
+		
+		GlStateManager.pushMatrix();
+		GlStat
