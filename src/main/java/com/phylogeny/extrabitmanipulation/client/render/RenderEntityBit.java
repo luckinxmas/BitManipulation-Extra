fa43b083507f896bed94d3565e1a4a3e5d2da77f
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-06-02 20:41:51 +0000

    Hound fixes

--- src/main/java/com/phylogeny/extrabitmanipulation/client/render/RenderEntityBit.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/render/RenderEntityBit.java
@@ -1,1 +1,23 @@
-package com.phylogeny.extrab
+package com.phylogeny.extrabitmanipulation.client.render;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.util.ResourceLocation;
+
+import com.phylogeny.extrabitmanipulation.client.ClientHelper;
+import com.phylogeny.extrabitmanipulation.entity.EntityBit;
+
+public class RenderEntityBit extends Render<EntityBit>
+{
+	
+	public RenderEntityBit(RenderManager renderManager)
+	{
+		super(renderManager);
+	}
+	
+	@Override
+	public void doRender(EntityBit entity, 
