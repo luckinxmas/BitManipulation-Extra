91b1edf0243f3a38112dd7d5b55fb77be80f04dc
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-06-06 10:32:45 +0000

    Update phpdoc

--- src/main/java/com/phylogeny/extrabitmanipulation/client/render/RenderState.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/render/RenderState.java
@@ -22,4 +22,22 @@ import net.minecraft.crash.CrashReport;
 import net.minecraft.crash.CrashReportCategory;
 import net.minecraft.crash.ICrashReportDetail;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStac
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ReportedException;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.model.pipeline.LightUtil;
+
+import org.lwjgl.opengl.GL11;
+
+import com.phylogeny.extrabitmanipulation.client.ClientHelper;
+import com.phylogeny.extrabitmanipulation.reference.Reference;
+
+public class RenderState
+{
+	private static final ResourceLocation RES_ITEM_GLINT = new ResourceLocation("textures/misc/enchanted_item_glint.png");
+	
+	public static void renderStateIntoGUI(final IBlockState state, int x, int y)
+	{
+		BlockModelShapes blockM
