420563c1e15e8898d3c68745df17e1244e2c9275
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-06-06 09:39:02 +0000

    name refactoring

--- src/main/java/com/phylogeny/extrabitmanipulation/client/render/RenderState.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/client/render/RenderState.java
@@ -10,4 +10,16 @@ import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.cli
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.renderer.texture.TextureUtil;
+import net.minecraft.client.renderer.tileentity.TileEntityItemStackRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.crash.CrashReportCategory;
+import net.minecraft.crash.ICrashReportDetail;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStac
