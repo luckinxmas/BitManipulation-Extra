f5252377ea9d023347a9a14cd1feacc59885b49b
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-06-14 09:56:28 +0000

    Update glossary.md

--- src/main/java/com/phylogeny/extrabitmanipulation/config/ConfigBitStack.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/config/ConfigBitStack.java
@@ -3,3 +3,21 @@ package com.phylogeny.extrabitmanipulation.config;
 import mod.chiselsandbits.api.APIExceptions.InvalidBitItem;
 import mod.chiselsandbits.api.IChiselAndBitsAPI;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemStack;
+
+import com.phylogeny.extrabitmanipulation.api.ChiselsAndBitsAPIAccess;
+
+public class ConfigBitStack extends ConfigBitToolSettingBase<ItemStack>
+{
+	private IBlockState valueDefault, stateDefault, stateDefaultDefault;
+	private String stringDeafult;
+	
+	public ConfigBitStack(ItemStack bitStackDefault)
+	{
+		super("", false, false);
+		defaultValue = bitStackDefault;
+	}
+	
+	public ConfigBitStack(String name, IBlockState bitBlockDefault, IBlockState defaultDefaultBitBlock, String stringDefault, IBlockState valueDefault)
+	{
+		this(name, false, false, bitBlockDefault, defaultDefaultBitBlock, stringDefault,
