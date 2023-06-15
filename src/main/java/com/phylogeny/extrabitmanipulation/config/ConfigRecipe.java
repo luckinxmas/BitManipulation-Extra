8fed44516827ee64984038a78cdf7593d8b1d7f2
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-06-15 11:24:13 +0000

    test 5

--- src/main/java/com/phylogeny/extrabitmanipulation/config/ConfigRecipe.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/config/ConfigRecipe.java
@@ -3,4 +3,27 @@ package com.phylogeny.extrabitmanipulation.config;
 public class ConfigRecipe extends ConfigNamed
 {
 	public boolean isEnabled, isShaped, useOreDictionary;
-	public String[]
+	public String[] recipe;
+	private boolean isEnabledDefault, isShapedDefault, useOreDictionaryDefault;
+	private String[] recipeDefault;
+	
+	public ConfigRecipe(String itemTitle, boolean isEnabledDefault, boolean isShapedDefault, boolean useOreDictionaryDefault, String... recipeDefault)
+	{
+		super(itemTitle);
+		this.isEnabledDefault = isEnabledDefault;
+		this.isShapedDefault = isShapedDefault;
+		this.useOreDictionaryDefault = useOreDictionaryDefault;
+		this.recipeDefault = recipeDefault;
+	}
+	
+	public boolean getIsEnabledDefault()
+	{
+		return isEnabledDefault;
+	}
+	
+	public boolean getIsShapedDefault()
+	{
+		return isShapedDefault;
+	}
+	
+	public boolean getUseOreDictionaryDe
