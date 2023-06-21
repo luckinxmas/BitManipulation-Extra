8391d7eab9d92ba0ffe93831a4b9c18e0d3c5393
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-06-21 20:25:17 +0000

    Remove logging

--- src/main/java/com/phylogeny/extrabitmanipulation/config/ConfigShapeRender.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/config/ConfigShapeRender.java
@@ -2,4 +2,17 @@ package com.phylogeny.extrabitmanipulation.config;
 
 public class ConfigShapeRender extends ConfigNamed
 {
-	public boolean renderOuterShape, renderIn
+	public boolean renderOuterShape, renderInnerShape;
+	private boolean renderOuterShapeDefault, renderInnerShapeDefault;
+	public float outerShapeAlpha, innerShapeAlpha, red, green, blue;
+	private int outerShapeAlphaDefault, innerShapeAlphaDefault, redDefault, greenDefault, blueDefault;
+	public float lineWidth;
+	private float lineWidthDefault;
+	
+	public ConfigShapeRender(String title, boolean renderOuterShapeDefault, boolean renderInnerShapeDefault, int outerShapeAlphaDefault,
+			int innerShapeAlphaDefault, int redDefault, int greenDefault, int blueDefault, float lineWidthDefault)
+	{
+		super(title);
+		this.renderOuterShapeDefault = renderOuterShapeDefault;
+		this.renderInnerShapeDefault = renderInnerShapeDefault;
+		
