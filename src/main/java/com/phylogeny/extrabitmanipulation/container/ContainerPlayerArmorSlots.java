f0dbd6bbdf2e97a5d2c84f16f61e72463f02dbc4
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-06-22 07:50:59 +0000

    tooltip

--- src/main/java/com/phylogeny/extrabitmanipulation/container/ContainerPlayerArmorSlots.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/container/ContainerPlayerArmorSlots.java
@@ -1,3 +1,17 @@
 package com.phylogeny.extrabitmanipulation.container;
 
-import net.minecraft.ent
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.ContainerPlayer;
+import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.inventory.InventoryCraftResult;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+
+import com.phylogeny.extrabitmanipulation.armor.capability.ChiseledArmorSlotsHandler;
+import com.phylogeny.extrabitmanipulation.armor.capability.IChiseledArmorSlotsHandler;
+
+public class ContainerPlayerArmorSlots extends ContainerPlayer
+{
