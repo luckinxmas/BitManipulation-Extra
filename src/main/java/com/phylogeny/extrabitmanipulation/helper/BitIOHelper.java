607b270f12974b32ad50c4d2921c11a8ffab8678
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-06-27 05:32:15 +0000

    Final submission

--- src/main/java/com/phylogeny/extrabitmanipulation/helper/BitIOHelper.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/helper/BitIOHelper.java
@@ -5,4 +5,23 @@ import io.netty.buffer.ByteBuf;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectI
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterInputStream;
+
+import mod.chiselsandbits.api.APIExceptions.InvalidBitItem;
+import mod.chiselsandbits.api.IBitBrush;
+import mod.chiselsandbits.api.IChiselAndBitsAPI;
+import mod.chiselsandbits.api.IMultiStateBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import 
