ed5c52564a7af0f33ec6f0f809f9acc4399ffe22
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-07-30 09:15:40 +0000

    version update

--- src/main/java/com/phylogeny/extrabitmanipulation/init/RecipesExtraBitManipulation.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/init/RecipesExtraBitManipulation.java
@@ -5,4 +5,23 @@ import mod.chiselsandbits.registry.ModItems;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.oredict.OreDictionary;
+
+import com.phylogeny.extrabitmanipulation.recipe.RecipeChiseledArmor;
+import com.phylogeny.extrabitmanipulation.reference.Configs;
+
+public class RecipesExtraBitManipulation
+{
+	public static void registerOres()
+	{
+		if (!Configs.disableDiamondNuggetOreDict)
+			OreDictionary.registerOre("nuggetDiamond", ItemsExtraBitManipulation.diamondNugget);
+	}
+	
+	@SubscribeEvent
+	void registerRecipes(RegistryEvent.Register<IRecip
