f73dbb13806669f3affc1a5291e6be16b820c1bc
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-08-03 05:51:59 +0000

    Fixed more tests

--- src/main/java/com/phylogeny/extrabitmanipulation/init/RenderLayersExtraBitManipulation.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/init/RenderLayersExtraBitManipulation.java
@@ -3,4 +3,16 @@ package com.phylogeny.extrabitmanipulation.init;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.m
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.entity.monster.EntityEvoker;
+import net.minecraft.entity.monster.EntityGiantZombie;
+import net.minecraft.entity.monster.EntityHusk;
+import net.minecraft.entity.monster.EntityIllusionIllager;
+import net.minecraft.entity.monster.EntityPigZombie;
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.monster.E
