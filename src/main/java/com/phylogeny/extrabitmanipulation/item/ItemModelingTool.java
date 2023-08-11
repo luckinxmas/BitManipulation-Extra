1281864f3676031bf9a13bfac3cc9e58a6572bbc
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-08-11 16:34:02 +0000

    Bump eslint from 7.20.0 to 7.21.0

--- src/main/java/com/phylogeny/extrabitmanipulation/item/ItemModelingTool.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/item/ItemModelingTool.java
@@ -8,4 +8,19 @@ import java.util.Map;
 import javax.annotation.Nullable;
 
 import mod.chiselsandbits.api.APIExceptions.CannotBeChiseled;
-import mod.chisels
+import mod.chiselsandbits.api.APIExceptions.InvalidBitItem;
+import mod.chiselsandbits.api.APIExceptions.SpaceOccupied;
+import mod.chiselsandbits.api.IBitAccess;
+import mod.chiselsandbits.api.IBitBrush;
+import mod.chiselsandbits.api.IChiselAndBitsAPI;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.uti
