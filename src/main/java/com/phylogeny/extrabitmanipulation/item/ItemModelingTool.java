994306219b7f87ea445022ec1f070994a74d0bdb
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-08-11 20:23:27 +0000

    Added basic logging

--- src/main/java/com/phylogeny/extrabitmanipulation/item/ItemModelingTool.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/item/ItemModelingTool.java
@@ -23,4 +23,19 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.uti
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.phylogeny.extrabitmanipulation.ExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.api.ChiselsAndBitsAPIAccess;
+import com.phylogeny.extrabitmanipulation.config.ConfigReplacementBits;
+import com.phylogeny.extrabitmanipulation.helper.BitIOHelper;
+import com.phylogeny.extrabitmanipulation.helper.BitInventoryHelper
