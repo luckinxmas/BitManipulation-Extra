6ed174efa37b62a070136ab45942b07a7d1806cd
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-08-19 22:09:33 +0000

    Added missing image

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketBlockInteraction.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketBlockInteraction.java
@@ -6,4 +6,33 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 
-public abstract class PacketBlockInteraction implem
+public abstract class PacketBlockInteraction implements IMessage
+{
+	protected BlockPos pos;
+	protected EnumFacing side;
+	protected Vec3d hit;
+	
+	public PacketBlockInteraction() {}
+	
+	public PacketBlockInteraction(BlockPos pos, EnumFacing side, Vec3d hit)
+	{
+		this.pos = pos;
+		this.side = side;
+		this.hit = hit;
+	}
+	
+	@Override
+	public void toBytes(ByteBuf buffer)
+	{
+		buffer.writeLong(pos.toLong());
+		buffer.writeInt(side.ordinal());
+		buffer.writeDouble(hit.x);
+		buffer.writeDouble(hit.y);
+		buffer.writeDouble(hit.z);
+	}
+	
+	@Override
+	public void fromBytes(ByteBuf buffer)
+	{
+		pos = BlockPos.fromLong(buffer.readLong());
+		side = EnumFacing.getFront(buffer.readIn
