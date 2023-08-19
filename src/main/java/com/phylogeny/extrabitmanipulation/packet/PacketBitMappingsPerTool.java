beda8a6b1ba0d46c254dae8af142a6b347448428
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-08-19 15:06:57 +0000

    Various enhancements

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketBitMappingsPerTool.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketBitMappingsPerTool.java
@@ -1,2 +1,17 @@
 package com.phylogeny.extrabitmanipulation.packet;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.IThreadListener;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+import com.phylogeny.extrabitmanipulation.reference.NBTKeys;
+
+public class PacketBitMappingsPerTool extends PacketBoolean
+{
+	public PacketBitMa
