39d0955785d0910931ed89e67ea630e3ca54cb52
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-08-22 08:04:48 +0000

    Fix import statements

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketChangeArmorItemList.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketChangeArmorItemList.java
@@ -11,4 +11,15 @@ import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.IThreadListener;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.util.Constants.NBT;
-import net.minecraftforge.fml.common.networ
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+
+import com.phylogeny.extrabitmanipulation.ExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.armor.ArmorItem;
+import com.phylogeny.extrabitmanipulation.armor.DataChiseledArmorPiece;
+import com.phylogeny.extrabitmanipulation.client.ClientHelper;
+import com.phylogeny.extrabitmanipulation.container.ContainerPlayerInventory;
+import com.phylogeny.extrab
