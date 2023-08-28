fde5e95eabb699b60153fdc0f928f599cfd476c3
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-08-28 18:10:09 +0000

    Update Screenshot

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketChangeChiseledArmorList.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketChangeChiseledArmorList.java
@@ -7,4 +7,19 @@ import com.phylogeny.extrabitmanipulation.armor.capability.ChiseledArmorSlotsHan
 import com.phylogeny.extrabitmanipulation.armor.capability.IChiseledArmorSlotsHandler;
 import com.phylogeny.extrabitmanipulation.client.GuiHelper;
 import com.phylogeny.extrabitmanipulation.client.gui.armor.GuiChiseledArmor;
-import com.phylogeny.extrabitm
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+import com.phylogeny.extrabitmanipulation.init.RenderLayersExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor.ArmorType;
+import com.phylogeny.extrabitmanipulation.reference.NBTKeys;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
+
+public abstract class PacketChangeChiseledArmorList extends PacketArmorSlotInt
+{
+	protected int armorItemIndex, selectedEntry;
+	protected boolean ref
