ba49e3b35cb006e0ba2b50b337c49559761e5dd8
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-09-12 11:53:42 +0000

    Create views.py

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketCreateModel.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketCreateModel.java
@@ -12,4 +12,27 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
-import com.phylogeny.extrabitmanipulation.he
+import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper.ModelWriteData;
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+import com.phylogeny.extrabitmanipulation.item.ItemModelingTool;
+
+public class PacketCreateModel extends PacketBlockInteraction implements IMessage
+{
+	private ModelWriteData modelingData = new ModelWriteData();
+	
+	public PacketCreateModel() {}
+	
+	public PacketCreateModel(BlockPos pos, EnumFacing side, ModelWriteData modelingData)
+	{
+		super(pos, side, new Vec3d(0, 0, 0));
+		this.modelingData = modelingData;
+	}
+	
+	@Override
+	public void toBytes(ByteBuf buffer)
+	{
+		super.toBytes(buffer);
+		modelingData.toBytes(buffer);
+	}
+	
+	@Over
