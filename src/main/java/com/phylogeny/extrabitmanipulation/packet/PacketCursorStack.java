86c6af9dd93682cb21813ca082d1d28d8ba09a2c
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-09-14 05:52:48 +0000

    Reorganized code

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketCursorStack.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketCursorStack.java
@@ -8,4 +8,34 @@ import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
-import net.minecraftforge.fml.common.network.s
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+public class PacketCursorStack implements IMessage
+{
+	private ItemStack stack;
+	
+	public PacketCursorStack() {}
+	
+	public PacketCursorStack(ItemStack stack)
+	{
+		this.stack = stack;
+	}
+	
+	@Override
+	public void toBytes(ByteBuf buffer)
+	{
+		ByteBufUtils.writeItemStack(buffer, stack);
+	}
+	
+	@Override
+	public void fromBytes(ByteBuf buffer)
+	{
+		stack = ByteBufUtils.readItemStack(buffer);
+	}
+	
+	public static class Handler implements IMessageHandler<PacketCursorStack, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketCursorStack message, final MessageContext ctx)
+		{
+			IThreadListener mainThread = (WorldServer) ctx
