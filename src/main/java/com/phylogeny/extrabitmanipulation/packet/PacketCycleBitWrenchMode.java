856657481ee57d5f463340525bdee54b2007f41d
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-09-22 05:48:55 +0000

    fixes #23

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketCycleBitWrenchMode.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketCycleBitWrenchMode.java
@@ -5,4 +5,24 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.IThreadListener;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
-import net.minecraftforge.fml.common.netwo
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+import com.phylogeny.extrabitmanipulation.item.ItemBitWrench;
+
+public class PacketCycleBitWrenchMode extends PacketBoolean
+{
+	public PacketCycleBitWrenchMode() {}
+	
+	public PacketCycleBitWrenchMode(boolean forward)
+	{
+		super(forward);
+	}
+	
+	public static class Handler implements IMessageHandler<PacketCycleBitWrenchMode, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketCycleBitWrenchMode message, final MessageContext ctx)
+		{
+			IThreadListener mainThread = (WorldServer) ctx.getServerHandler().player.worl
