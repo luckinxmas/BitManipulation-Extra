356ce4f9742bd995bbccf792bd9d8068c15aac45
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-09-25 07:16:53 +0000

    Remove assert

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketOpenBitMappingGui.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketOpenBitMappingGui.java
@@ -8,4 +8,23 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
-import com.phylogeny.extrabitmanipulation.E
+import com.phylogeny.extrabitmanipulation.ExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+import com.phylogeny.extrabitmanipulation.reference.GuiIDs;
+
+public class PacketOpenBitMappingGui extends PacketEmpty
+{
+	public PacketOpenBitMappingGui() {}
+	
+	public static class Handler implements IMessageHandler<PacketOpenBitMappingGui, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketOpenBitMappingGui message, final MessageContext ctx)
+		{
+			IThreadListener mainThread = (WorldServer) ctx.getServerHandler().player.world;
+			mainThread.addScheduledTask(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					EntityPlayer player = ctx.getServerHandler().pla
