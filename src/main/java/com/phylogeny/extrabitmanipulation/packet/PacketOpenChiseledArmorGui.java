89d2e877eb29ba15d27f3e62809710cc44584dd5
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-09-25 08:13:05 +0000

    Added license information.

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketOpenChiseledArmorGui.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketOpenChiseledArmorGui.java
@@ -4,4 +4,22 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IThreadListener;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.ExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.reference.GuiIDs;
+
+public class PacketOpenChiseledArmorGui extends PacketEmpty
+{
+	public PacketOpenChiseledArmorGui() {}
+	
+	public static class Handler implements IMessageHandler<PacketOpenChiseledArmorGui, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketOpenChiseledArmorGui message, final MessageContext ctx)
+		{
+			IThreadListener mainThread = (WorldServer) ctx.getServerHandler().player.world;
+			mainThread.addScheduledTask(new Runnable()
+			{
+				@Ov
