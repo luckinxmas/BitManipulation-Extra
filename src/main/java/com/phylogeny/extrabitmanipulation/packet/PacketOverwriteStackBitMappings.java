b1d5c5da40a791a35a2bdf2f2d9803c05ee7edce
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-09-25 14:58:21 +0000

    final update

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketOverwriteStackBitMappings.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketOverwriteStackBitMappings.java
@@ -10,4 +10,20 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IThreadListener;
 import net.minecraft.world.WorldServer;
-import net.minecraftfo
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.helper.BitIOHelper;
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+
+public class PacketOverwriteStackBitMappings extends PacketBitMapIO
+{
+	private Map<IBlockState, IBitBrush> bitMap;
+	
+	public PacketOverwriteStackBitMappings() {}
+	
+	public PacketOverwriteStackBitMappings(Map<IBlockState, IBitBrush> bitMap, String nbtKey, boolean saveStatesById)
+	{
+		super(nbtKey, saveStatesById);
+		this.bitMap = bit
