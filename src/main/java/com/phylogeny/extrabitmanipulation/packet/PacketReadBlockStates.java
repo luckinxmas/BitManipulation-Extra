80b1de0479620555480647064cead8adf76bcada
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-09-28 15:16:34 +0000

    fix master

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketReadBlockStates.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketReadBlockStates.java
@@ -15,4 +15,27 @@ import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 import com.phylogeny.extrabitmanipulation.helper.BitAreaHelper;
 import com.phylogeny.extrabitmanipulation.helper.BitIOHelper;
-import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper.Mo
+import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper.ModelReadData;
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+
+public class PacketReadBlockStates extends PacketBlockInteraction implements IMessage
+{
+	private Vec3i drawnStartPoint;
+	private ModelReadData modelingData = new ModelReadData();
+	
+	public PacketReadBlockStates() {}
+	
+	public PacketReadBlockStates(BlockPos pos, Vec3d hit, Vec3i drawnStartPoint, ModelReadData modelingData)
+	{
+		super(pos, EnumFacing.UP, hit);
+		this.drawnStartPoint = drawnStartPoint;
+		this.modelingData = modelingData;
+	}
+	
+	@Override
+	public void toBytes(ByteBuf buffer)
+	{
+		super.toBytes(buffer);
+		if (BitIOHelper.notNullToBuffer(buffer, drawnStartPoint))
+		{
+			buffer.writeInt
