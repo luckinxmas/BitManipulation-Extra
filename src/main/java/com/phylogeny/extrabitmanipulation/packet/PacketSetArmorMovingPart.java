b88f90d68db1bf0622967b1faf2f499870898884
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-10-01 15:54:45 +0000

    Code adjustments

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetArmorMovingPart.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetArmorMovingPart.java
@@ -3,4 +3,20 @@ package com.phylogeny.extrabitmanipulation.packet;
 import javax.annotation.Nullable;
 
 import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
-import com.phylogeny.extra
+import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor.ArmorType;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IThreadListener;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+public class PacketSetArmorMovingPart extends PacketArmorSlotInt
+{
+	public PacketSetArmorMovingPart() {}
+	
+	public PacketSetArmorMovingPart(int partIndex, @Nullable ArmorType armorType, int indexArmorSet)
+	{
+		super(armorType, indexArmorSet, par
