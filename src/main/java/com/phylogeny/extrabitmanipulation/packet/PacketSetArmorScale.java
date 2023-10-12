a6fc17cf6537701d845f400ef7c9f19e74cc407e
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-10-12 16:04:51 +0000

    dependency

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetArmorScale.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetArmorScale.java
@@ -13,4 +13,25 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
-public class PacketSetArmorScale exten
+public class PacketSetArmorScale extends PacketArmorSlotInt
+{
+	public PacketSetArmorScale() {}
+	
+	public PacketSetArmorScale(int scale, @Nullable ArmorType armorType, int indexArmorSet)
+	{
+		super(armorType, indexArmorSet, scale);
+	}
+	
+	public static class Handler implements IMessageHandler<PacketSetArmorScale, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketSetArmorScale message, final MessageContext ctx)
+		{
+			IThreadListener mainThread = (WorldServer) ctx.getServerHandler().player.world;
+			mainThread.addScheduledTask(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					EntityPlayer player = ctx.getServerHandler().player;
+					ItemStack stack = getArmorStack(player, message);
