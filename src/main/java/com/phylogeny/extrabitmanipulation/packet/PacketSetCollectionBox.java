379cd1a471962cba71c6a48800b56bbf766ff03f
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-10-16 11:00:06 +0000

    Added new unit tests

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetCollectionBox.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetCollectionBox.java
@@ -8,4 +8,20 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IThreadListener;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.WorldSer
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor;
+
+public class PacketSetCollectionBox extends PacketBlockInteraction
+{
+	private float playerYaw;
+	private boolean useBitGrid;
+	private EnumFacing facingBox;
+	
+	public PacketSetCollectionBox() {}
+	
+	public PacketSetCollectionBox(float playerYaw, boolean useBitGrid, EnumFacing facingBox, BlockPos pos, EnumFacing facingPlacement, Vec3d hit
