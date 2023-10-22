fa07137f246bc5e2e3fd7d41f6011db3befa43c9
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-10-22 05:43:51 +0000

    Shrink ashmem directly through shmem_fallocate

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetDirection.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetDirection.java
@@ -7,4 +7,27 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
-import com.phylogeny.extrabitmanipulation
+import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
+
+public class PacketSetDirection extends PacketInt
+{
+	public PacketSetDirection() {}
+	
+	public PacketSetDirection(int direction)
+	{
+		super(direction);
+	}
+	
+	public static class Handler implements IMessageHandler<PacketSetDirection, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketSetDirection message, final MessageContext ctx)
+		{
+			IThreadListener mainThread = (WorldServer) ctx.getServerHandler().player.world;
+			mainThread.addScheduledTask(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					EntityPlayer player = ctx.getServerHandler().player;
+					BitToolSettingsHelper.setDirection(player, 
