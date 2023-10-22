0ed1329800c133015728ba0040d9d51293fbdbcd
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-10-22 15:30:48 +0000

    msm: cpufreq: increase priority of thread that increases frequencies

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetHollowShape.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetHollowShape.java
@@ -17,4 +17,30 @@ public class PacketSetHollowShape implements IMessage
 	
 	public PacketSetHollowShape() {}
 	
-	public PacketSetHollow
+	public PacketSetHollowShape(boolean hollowShape, boolean isWire)
+	{
+		this.hollowShape = hollowShape;
+		this.isWire = isWire;
+	}
+	
+	@Override
+	public void toBytes(ByteBuf buffer)
+	{
+		buffer.writeBoolean(hollowShape);
+		buffer.writeBoolean(isWire);
+	}
+	
+	@Override
+	public void fromBytes(ByteBuf buffer)
+	{
+		hollowShape = buffer.readBoolean();
+		isWire = buffer.readBoolean();
+	}
+	
+	public static class Handler implements IMessageHandler<PacketSetHollowShape, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketSetHollowShape message, final MessageContext ctx)
+		{
+			IThreadListener mainThread = (WorldServer) ctx.getServerHandler().player.world;
+			mainThread.addScheduledTask(new 
