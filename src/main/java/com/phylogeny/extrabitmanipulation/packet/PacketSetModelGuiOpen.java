0893e1389d32d921abcd4797dd6db462c8b33fd0
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-10-22 22:23:54 +0000

    :recycle: bump deps

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetModelGuiOpen.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetModelGuiOpen.java
@@ -4,4 +4,24 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IThreadListener;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
-import net.minecraftforge.fml.common.network.sim
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
+
+public class PacketSetModelGuiOpen extends PacketBoolean
+{
+	public PacketSetModelGuiOpen() {}
+	
+	public PacketSetModelGuiOpen(boolean openGui)
+	{
+		super(openGui);
+	}
+	
+	public static class Handler implements IMessageHandler<PacketSetModelGuiOpen, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketSetModelGuiOpen message, final MessageContext ctx)
+		{
+			IThreadListener mainThread = (WorldServer) ctx.getServerHandler().player.world;
+			mainThread.addScheduledTask(n
