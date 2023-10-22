fc9e99afa2661b538a87d71d182afcb13cfbda1b
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-10-22 14:49:11 +0000

    more documentation work

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetHollowShape.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetHollowShape.java
@@ -1,1 +1,20 @@
-package com.phylogeny.extrabitmanipulation.packet
+package com.phylogeny.extrabitmanipulation.packet;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.IThreadListener;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
+
+public class PacketSetHollowShape implements IMessage
+{
+	private boolean isWire;
+	private boolean hollowShape;
+	
+	public PacketSetHollowShape() {}
+	
+	public PacketSetHollow
