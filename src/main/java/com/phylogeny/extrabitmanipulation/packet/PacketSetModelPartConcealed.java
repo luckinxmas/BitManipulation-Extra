4c24b6ffbaf2771a6932be341dba1424373a99c6
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-10-23 12:09:06 +0000

    include/linux/poison.h: fix LIST_POISON{1,2} offset

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetModelPartConcealed.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetModelPartConcealed.java
@@ -5,4 +5,20 @@ import javax.annotation.Nullable;
 import com.phylogeny.extrabitmanipulation.armor.ModelPartConcealer;
 import com.phylogeny.extrabitmanipulation.armor.capability.ChiseledArmorSlotsHandler;
 import com.phylogeny.extrabitmanipulation.armor.capability.IChiseledArmorSlotsHandler;
-import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor.
+import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor.ArmorType;
+import com.phylogeny.extrabitmanipulation.item.ItemChiseledArmor.ModelMovingPart;
+
+import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.IThreadListener;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+public class PacketSetModelPartConcealed extends PacketArmorSlotInt
+{
+	private boolean isOverlay, remove;
+	
