bfff25598992fbc1e22066cdc02f4ec50c9672fa
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-10-30 20:33:39 +0000

    doxygen fixes

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetModelSnapMode.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetModelSnapMode.java
@@ -1,3 +1,24 @@
 package com.phylogeny.extrabitmanipulation.packet;
 
-import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.IThreadListener;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
+
+public class PacketSetModelSnapMode extends PacketInt
+{
+	public PacketSetModelSnapMode() {}
+	
+	public PacketSetModelSnapMode(int mode)
+	{
+		super(mode);
+	}
+	
+	public static class Handler implements IMessageHandler<PacketSetModelSnapMode, IMessage>
+	{
+		@Override
+		pu
