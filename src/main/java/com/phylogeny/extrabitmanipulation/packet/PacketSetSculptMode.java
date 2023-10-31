7ec040b91e5b5e08b096e6b7f40c3402330ebd91
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-10-31 13:30:46 +0000

    bing

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetSculptMode.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetSculptMode.java
@@ -2,4 +2,25 @@ package com.phylogeny.extrabitmanipulation.packet;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IThreadListener;
-import net.minecraft.world.Worl
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
+
+public class PacketSetSculptMode extends PacketInt
+{
+	public PacketSetSculptMode() {}
+	
+	public PacketSetSculptMode(int mode)
+	{
+		super(mode);
+	}
+	
+	public static class Handler implements IMessageHandler<PacketSetSculptMode, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketSetSculptMode message, final MessageContext ctx)
+		{
+			IThreadListener main
