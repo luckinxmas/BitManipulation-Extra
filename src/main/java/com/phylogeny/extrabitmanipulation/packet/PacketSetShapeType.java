b964a0654785333b0d752f0097f4a15f7286d805
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-11-01 21:28:07 +0000

    Fix image name

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetShapeType.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetShapeType.java
@@ -4,4 +4,31 @@ import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IThreadListener;
 import net.minecraft.world.WorldServer;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessa
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
+
+public class PacketSetShapeType implements IMessage
+{
+	private boolean isCurved;
+	private int shapeType;
+	
+	public PacketSetShapeType() {}
+	
+	public PacketSetShapeType(boolean isCurved, int shapeType)
+	{
+		this.isCurved = isCurved;
+		this.shapeType = shapeType;
+	}
+	
+	@Override
+	public void toBytes(ByteBuf buffer)
+	{
+		buffer.writeBoolean(isCurved);
+		buffer.writeInt(shapeType);
+	}
+	
+	@Override
+	public void 
