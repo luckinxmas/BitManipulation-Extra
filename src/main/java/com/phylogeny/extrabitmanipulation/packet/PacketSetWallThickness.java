55b4f4dfaf6a52883ccb4a4c1668df2e7c8cbc86
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-11-02 19:30:23 +0000

    Updated Screenshot

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetWallThickness.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetWallThickness.java
@@ -11,4 +11,26 @@ import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
 
 public class PacketSetWallThickness extends PacketInt
 {
-	public PacketSetW
+	public PacketSetWallThickness() {}
+	
+	public PacketSetWallThickness(int wallThickness)
+	{
+		super(wallThickness);
+	}
+	
+	public static class Handler implements IMessageHandler<PacketSetWallThickness, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketSetWallThickness message, final MessageContext ctx)
+		{
+			IThreadListener mainThread = (WorldServer) ctx.getServerHandler().player.world;
+			mainThread.addScheduledTask(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					EntityPlayer player = ctx.getServerHandler().player;
+					BitToolSettingsHelper.setWallThickness(player, player.getHeldItemMainhand(), message.value, null);
+				}
+			});
+			retu
