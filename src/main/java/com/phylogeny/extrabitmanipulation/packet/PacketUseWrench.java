018946d788549560842a17a7f1a8d2bd4fb86f4d
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-11-02 20:48:02 +0000

    Rename index.php to index.html

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketUseWrench.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketUseWrench.java
@@ -8,4 +8,20 @@ import net.minecraft.util.IThreadListener;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.WorldServer;
-import net.minecraftforge.fml.common.network
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
+import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+
+import com.phylogeny.extrabitmanipulation.helper.ItemStackHelper;
+import com.phylogeny.extrabitmanipulation.item.ItemBitWrench;
+
+public class PacketUseWrench extends PacketBlockInteraction implements IMessage
+{
+	private boolean bitRequirement, invertDirection;
+	
+	public PacketUseWrench() {}
+	
+	public PacketUseWrench(BlockPos pos, EnumFacing side, boolean bitRequirement, boolean invertDirection)
+	{
+		super(pos, side, new Vec3d(0, 0, 0));
+		this.bitRequirement
