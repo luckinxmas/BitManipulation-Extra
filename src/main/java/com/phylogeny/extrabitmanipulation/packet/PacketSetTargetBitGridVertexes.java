aa219456e338360ab71387e72f45aaf7868c484d
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-11-02 10:04:12 +0000

    new edit

--- src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetTargetBitGridVertexes.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/packet/PacketSetTargetBitGridVertexes.java
@@ -7,4 +7,26 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
-import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsH
+import com.phylogeny.extrabitmanipulation.helper.BitToolSettingsHelper;
+
+public class PacketSetTargetBitGridVertexes extends PacketBoolean
+{
+	public PacketSetTargetBitGridVertexes() {}
+	
+	public PacketSetTargetBitGridVertexes(boolean targetBitGridVertexes)
+	{
+		super(targetBitGridVertexes);
+	}
+	
+	public static class Handler implements IMessageHandler<PacketSetTargetBitGridVertexes, IMessage>
+	{
+		@Override
+		public IMessage onMessage(final PacketSetTargetBitGridVertexes message, final MessageContext ctx)
+		{
+			IThreadListener mainThread = (WorldServer) ctx.getServerHandler().player.world;
+			mainThread.addScheduledTask(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					EntityPlayer player = ctx.getServerHa
