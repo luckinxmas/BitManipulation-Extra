f20dbecf7dd3712e3c2df8ae897c8c92f471e0fa
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-11-05 21:00:30 +0000

    Examples: Clean up.

--- src/main/java/com/phylogeny/extrabitmanipulation/proxy/ProxyClient.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/proxy/ProxyClient.java
@@ -2,4 +2,16 @@ package com.phylogeny.extrabitmanipulation.proxy;
 
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.cl
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import com.phylogeny.extrabitmanipulation.client.ClientEventHandler;
+import com.phylogeny.extrabitmanipulation.client.ClientHelper;
+import com.phylogeny.extrabitmanipulation.client.render.RenderEntityBit;
+import com.phylogeny.extrabitmanipulation.entity.EntityBit;
+import com.phylogeny.ex
