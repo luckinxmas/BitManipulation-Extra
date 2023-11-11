6dcacd0d0ad326c364ddb0a7fff724e575963f1a
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-11-11 14:02:36 +0000

    Update startup.sh

--- src/main/java/com/phylogeny/extrabitmanipulation/proxy/ProxyCommon.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/proxy/ProxyCommon.java
@@ -7,4 +7,15 @@ import net.minecraft.dispenser.IPosition;
 import net.minecraft.entity.IProjectile;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.IGuiHandler;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+
+import com.phylogeny.extrabitmanipulation.ExtraBitManipulation;
+import com.phylogeny.extrabitmanipulation.armor.capability.ChiseledArmorSlotsEventH
