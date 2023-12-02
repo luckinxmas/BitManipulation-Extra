866961ae77f1b8b83469a89bcab463d8be05252c
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-12-02 16:42:51 +0000

    Update humans.txt

---
+++ src/main/java/com/phylogeny/extrabitmanipulation/reference/CustomNPCsReferences.java
@@ -0,0 +1,8 @@
+package com.phylogeny.extrabitmanipulation.reference;
+
+public class CustomNPCsReferences
+{
+	public static final String MOD_ID = "customnpcs";
+	public static boolean isLoaded;
+	
+}
---
+++ src/main/java/com/phylogeny/extrabitmanipulation/reference/GuiIDs.java
@@ -0,0 +1,12 @@
+package com.phylogeny.extrabitmanipulation.reference;
+
+public enum GuiIDs
+{
+	BIT_MAPPING, CHISELED_ARMOR, CHISELED_ARMOR_SLOTS;
+	
+	public int getID()
+	{
+		return this.ordinal();
+	}
+	
+}
