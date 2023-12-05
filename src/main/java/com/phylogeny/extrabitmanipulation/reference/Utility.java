d7829af512138da22738ce049ee263a6dc339436
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-12-05 20:46:05 +0000

    close #1

--- src/main/java/com/phylogeny/extrabitmanipulation/reference/Utility.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/reference/Utility.java
@@ -1,3 +1,8 @@
 package com.phylogeny.extrabitmanipulation.reference;
 
-public class Utilit
+public class Utility
+{
+	public static final float PIXEL_F = 1 / 16.0F;
+	public static final double PIXEL_D = 1 / 16.0D;
+	
+}
---
+++ src/main/java/com/phylogeny/extrabitmanipulation/shape/AsymmetricalShape.java
@@ -0,0 +1,57 @@
+
+package com.phylogeny.extrabitmanipulation.shape;
+
+import net.minecraft.util.math.AxisAlignedBB;
+
+public abstract class AsymmetricalShape extends Shape
+{
+	protected float a, b, c, aInset, bInset, cInset;
+	protected boolean isEquilateral;
+	
+	public void init(float centerX, float centerY, float centerZ, float a, float b, float c,
+			int direction, boolean sculptHollowShape, float wallThickness, boolean openEnds)
+	{
+		init(centerX, centerY, centerZ, direction, sculptHollowShape, wallThickness, openEnds);
+		float v;
+		if (this.direction > 1)
+		{
+			if (this.direction > 3)
+			{
+				v = a;
+				a = b;
+				b = v;
+			}
+			else
+			{
+				v = c;
+				c = b;
+				b = v;
+			}
+		}
+		this.a = a; 
+		this.b = b;
+		this.c = c;
+		aInset = reduceLength(this.a);
+		bInset = reduceLength(this.b);
+		cInset = reduceLength(this.c);
+	}
+	
+	public void setEquilateral(boolean isEquilateral)
+	{
+		this.isEquilateral = isEquilateral;
+	}
+	
+	@Override
+	protected AxisAlignedBB getBoundingBox()
+	{
+		return new AxisAlignedBB(centerX - a, centerY - b, centerZ - c, centerX + a, centerY + b, centerZ + c);
+	}
+	
+	protected boolean isPointInEllipse(float dv1, float dv2, float s1, float s2)
+	{
+		float v1 = dv1 / s1;
+		float v2 = dv2 / s2;
+		return v1 * v1 + v2 * v2 <= 1;
+	}
+	
+}
