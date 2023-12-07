b3296a227cb5d86c9616056f71761df53f4ae916
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-12-07 19:51:50 +0000

    updated VERSION file

--- src/main/java/com/phylogeny/extrabitmanipulation/shape/Cube.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/shape/Cube.java
@@ -1,1 +1,21 @@
-package com.phylogeny.extrabitma
+package com.phylogeny.extrabitmanipulation.shape;
+
+import net.minecraft.util.math.BlockPos;
+
+import com.phylogeny.extrabitmanipulation.reference.Utility;
+
+public class Cube extends SymmetricalShape
+{
+	
+	@Override
+	public boolean isPointInsideShape(BlockPos pos, int i, int j, int k)
+	{
+		float x = pos.getX() + i * Utility.PIXEL_F;
+		float y = pos.getY() + j * Utility.PIXEL_F;
+		float z = pos.getZ() + k * Utility.PIXEL_F;
+		return isPointInsideisCube(x, y, z, semiDiameter) && (!sculptHollowShape || !isPointInsideisCube(x, y, z, semiDiameterInset));
+	}
+	
+	private boolean isPointInsideisCube(float x, float y, float z, float semiDiameter)
+	{
+		return x <= centerX + semiDiameter && x >= centerX 
