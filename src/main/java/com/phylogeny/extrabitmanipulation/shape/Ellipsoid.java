81a4120201990a8870db3c7b28692cccbe1ee1db
luckinxmas <121597159+luckinxmas@users.noreply.github.com>
2023-12-17 13:10:23 +0000

    merge the kinstance-redesign branch back to trunk.

--- src/main/java/com/phylogeny/extrabitmanipulation/shape/Ellipsoid.java
+++ src/main/java/com/phylogeny/extrabitmanipulation/shape/Ellipsoid.java
@@ -15,4 +15,12 @@ public class Ellipsoid extends AsymmetricalShape
 		return sculptHollowShape ? inShape && !isPointInsideisEllipsoid(dx, dy, dz, aInset, bInset, cInset) : inShape;
 	}
 	
-	private boolean isPointInsideisEllipsoid(float dx, float dy, float dz, 
+	private boolean isPointInsideisEllipsoid(float dx, float dy, float dz, float a, float b, float c)
+	{
+		float vx = dx / a;
+		float vy = dy / b;
+		float vz = dz / c;
+		return vx * vx + vy * vy + vz * vz <= 1;
+	}
+	
+}
---
+++ src/main/java/com/phylogeny/extrabitmanipulation/shape/PyramidRectangular.java
@@ -0,0 +1,8 @@
+package com.phylogeny.extrabitmanipulation.shape;
+
+public class PyramidRectangular extends SlopedAsymmetricalShape
+{
+	
+	@Override
+	protected boolean isPointIn2DShape(float val, float semiDiameter1, float semiDiameter2, float dv1, float dv2)
+	{
